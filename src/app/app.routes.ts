4412b251a5320129abfab1a5c1bc375286dacbb9
josefandino <josefandinod@gmail.com>
2024-04-23 19:18:47 -0500

    Update routes in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,8 @@ import AuthComponent from './components/auth/auth.component';
 import CrudComponent from './components/crud/crud.component';
 
 export const routes: Routes = [
-<<<<<<< HEAD
     { path: '', component: DashboardComponent },
     { path: 'auth', component: AuthComponent },
+    { path: 'crud', component: CrudComponent },
     { path: '**', redirectTo: '', pathMatch: 'full' }
-=======
-    {path:'', component:DashboardComponent},    
-    {path:'auth', component:AuthComponent},
-    {path:'crud', component:CrudComponent},    
-    {path:'', redirectTo:'', pathMatch:'full'}
->>>>>>> 26c7be3be6e96a125250940bc7f155bf8866b91e
 ];
